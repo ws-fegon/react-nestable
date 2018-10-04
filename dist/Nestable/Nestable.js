f82147a929b03ecf4250a232062b3e7bfdfd95d5
Sergey <ws.fegon@gmail.com>
2018-10-04 15:22:34 +0300

    Update Nestable.js

--- dist/Nestable/Nestable.js
+++ dist/Nestable/Nestable.js
@@ -590,7 +590,7 @@ var Nestable = function (_Component) {
       var group = this.props.group;
       var dragItem = this.state.dragItem;
 
-      var el = document.querySelector('.nestable-' + group + ' .nestable-item-' + dragItem.id);
+      var el = document.querySelector('.nestable-' + group + ' .nestable-item-' + dragItem.id.replace('.', '\\.'));
 
       var listStyles = {};
       if (el) {
